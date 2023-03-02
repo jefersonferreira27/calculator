44dc70e152a01d43d7d73253ff60c583837a5491
Jeferson Ferreira <jefersonferreira27@outlook.com>
2023-03-02 00:24:58 -0300

    Alter

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
+//Inserior os números da calculadora//
 function insert(num){
    let number = document.getElementById('result').innerHTML;
    document.getElementById('result').innerHTML = number+num;
 }   
+
+//Limpar os números da calculadora//
+function clean(){
+   let result = document.getElementById('result');
+   result.innerHTML = '';
+}
+
